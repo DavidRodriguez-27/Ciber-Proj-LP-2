48d0779461e66309410fb3729aeb5e4950438202
CarlosDelgadilloS <carlos.delgadillo.s@uni.edu.pe>
2024-10-18 22:06:51 -0500

    Se subió el proyecto

---
+++ proyectoGrupo4/src/main/java/com/cibertec/proyectogrupo4/ProyectoGrupo4Application.java
@@ -0,0 +1,13 @@
+package com.cibertec.proyectogrupo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGrupo4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoGrupo4Application.class, args);
+    }
+
+}
---
+++ proyectoGrupo4/src/test/java/com/cibertec/proyectogrupo4/ProyectoGrupo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.proyectogrupo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGrupo4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
